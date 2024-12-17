59e27d5f2af37e3900b3a7cd193e81561bd24b86
Jiri Daněk <jdanek@redhat.com>
2024-12-17 12:31:02 +0100

    initial commit for the python transpiler

---
+++ ods_ci/utils/scripts/transpile_to_python.py
@@ -0,0 +1,436 @@
+#!/usr/bin/env python3
+
+"""
+Examples
+Input:
+poetry run ods_ci/utils/scripts/fetch_tests.py --test-repo git@github.com:red-hat-data-services/ods-ci.git --ref1 releases/2.8.0 --ref2-auto true --selector-attribute creatordate -A new-arg-file.txt
+Output:
+---| Computing differences |----
+Done. Found 30 new tests in releases/2.8.0 which were not present in origin/releases/2.7.0
+
+Input:
+poetry run ods_ci/utils/scripts/fetch_tests.py --test-repo git@github.com:red-hat-data-services/ods-ci.git --ref1 master  --ref2-auto true --selector-attribute creatordate -A new-arg-file.txt
+Output:
+---| Computing differences |----
+Done. Found 14 new tests in master which were not present in origin/releases/2.9.0
+
+"""
+
+import argparse
+import io
+import os
+import pathlib
+import re
+import shutil
+import unittest
+
+import robot.running.namespace
+from robot.model import SuiteVisitor
+from robot.running import TestSuiteBuilder
+import robot.variables.scopes
+import robot.conf.settings
+import robot.api.parsing
+import robot.api.interfaces
+import robot.running.model
+
+from ods_ci.utils.scripts.util import execute_command
+
+
+class TestCasesFinder(SuiteVisitor):
+    def __init__(self):
+        self.tests = []
+
+    def visit_test(self, test):
+        self.tests.append(test)
+
+
+def get_repository(test_repo):
+    """
+    If $test_repo is a remote repo, the function clones it in "ods-ci-temp" directory.
+    If $test_repo is a local path, the function checks the path exists.
+    """
+    repo_local_path = "./ods_ci/ods-ci-temp"
+    cloned = False
+    if "http" in test_repo or "git@" in test_repo:
+        print("Cloning repo ", test_repo)
+        cloned = True
+        execute_command(f"git clone {test_repo} {repo_local_path}")
+    elif not os.path.exists(test_repo):
+        raise FileNotFoundError(f"local path {test_repo} was not found")
+    else:
+        print("Using local repo ", test_repo)
+        repo_local_path = test_repo
+    return repo_local_path, cloned
+
+
+def checkout_repository(ref):
+    """
+    Checkouts the repository at current directory to the given branch/commit ($ref)
+    """
+    execute_command(f"git checkout {ref}")
+    execute_command("git checkout")
+
+
+def get_branch(ref_to_exclude, selector_attribute):
+    """
+    List the remote branches and sort by selector_attribute date (ASC order), exclude $ref_to_exclude and get latest
+    """
+    ref_to_exclude_esc = ref_to_exclude.replace("/", r"\/")
+    cmd = f"git branch -r --sort={selector_attribute} | grep releases/"
+    if "master" not in ref_to_exclude and "main" not in ref_to_exclude:
+        cmd += rf" | sed  's/.*{ref_to_exclude_esc}$/current/g' |  grep -zPo '[\S\s]+(?=current)'"
+    ret = execute_command(cmd)
+    branches = ret.split(" ")
+    branch = branches[-1].split("\x00")[0].strip().replace("\n", "")
+    if not branch or "fatal:" in branch:
+        raise Exception("Failed to auto-selecting ref_2 branch.")
+    print(f"Done. {branch} branch selected as ref_2")
+    return branch
+
+
+def extract_test_cases_from_ref(repo_local_path, ref, auto=False, selector_attribute=None, ref_to_exclude=None):
+    """
+    Navigate to the $test_repo directory, checkouts the target branch/commit ($ref) and extracts
+    the test case titles leveraging RobotFramework TestSuiteBuilder() and TestCasesFinder() classes
+    """
+    curr_dir = os.getcwd()
+    try:
+        os.chdir(repo_local_path)
+        if auto:
+            print("\n---| Auto-selecting ref_2 branch")
+            ref = get_branch(ref_to_exclude, selector_attribute)
+        print(f"\n---| Extracting test cases from {ref} branch/commit |---")
+        # checkout_repository(ref)
+        builder = TestSuiteBuilder()
+        testsuite = builder.build("tests/")
+        finder = TestCasesFinder()
+        tests = []
+        testsuite.visit(finder)
+        for test in finder.tests:
+            # print (f'"{test.tags}"') # for future reference in order to fetch test tags
+            tests.append(test.name)
+        print(f"\nDone. Found {len(tests)} test cases")
+    except Exception as err:
+        print(err)
+        os.chdir(curr_dir)
+        raise
+    os.chdir(curr_dir)
+    return tests, ref
+
+
+def generate_rf_argument_file(tests, output_filepath):
+    """
+    Writes the RobotFramework argument file containing the test selection args
+    to include the extracted new tests in previous stage of this script.
+    """
+    content = ""
+    for testname in tests:
+        content += f"--test {testname.strip()}\n"
+    try:
+        with open(output_filepath, "w") as argfile:
+            argfile.write(content)
+    except Exception as err:
+        print("Failed to generate argument file")
+        print(err)
+
+
+def extract_new_test_cases(test_repo, ref_1, ref_2, ref_2_auto, selector_attribute, output_argument_file):
+    """
+    Wrapping function for all the new tests extraction stages.
+    """
+    repo_local_path, cloned = get_repository(test_repo)
+    tests_1, _ = extract_test_cases_from_ref(repo_local_path, ref_1)
+    tests_2, ref_2 = extract_test_cases_from_ref(repo_local_path, ref_2, ref_2_auto, selector_attribute, ref_1)
+    print("\n---| Computing differences |----")
+    new_tests = list(set(tests_1) - set(tests_2))
+    if len(new_tests) == 0:
+        print(f"[WARN] Done. No new tests found in {ref_1} with respect to {ref_2}!")
+        print("Skip argument file creation")
+    else:
+        print(f"Done. Found {len(new_tests)} new tests in {ref_1} which were not present in {ref_2}")
+        if output_argument_file is not None:
+            print("\n---| Generating RobotFramework arguments file |----")
+            generate_rf_argument_file(new_tests, output_argument_file)
+            print("Done.")
+    if cloned:
+        print(f"\n---| Deleting cloned repo in {repo_local_path} |----")
+        shutil.rmtree(repo_local_path)
+
+
+class Args(argparse.Namespace):
+    output_argument_file: argparse.FileType('w')
+    test_repo: str
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        usage=argparse.SUPPRESS,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="Script to fetch newly added test cases",
+    )
+
+    parser.add_argument(
+        "-A",
+        "--output-argument-file",
+        help="generate argument file for RobotFramework to include test cases. It expects to receive a file path",
+        action="store",
+        dest="output_argument_file",
+        default=None,
+    )
+    parser.add_argument(
+        "--test-repo",
+        help="ODS-CI repository. It accepts either local path or URL",
+        action="store",
+        dest="test_repo",
+        default="https://github.com/red-hat-data-services/ods-ci",
+    )
+
+    args = parser.parse_args()
+
+    extract_new_test_cases(
+        args.test_repo,
+        args.ref_1,
+        args.ref_2,
+        args.ref_2_auto,
+        args.selector_attribute,
+        args.output_argument_file,
+    )
+
+import ast
+
+
+class TestNamePrinter(ast.NodeVisitor):
+
+    def visit_File(self, node):
+        print(f"File '{node.source}' has following tests:")
+        # Must call `generic_visit` to visit also child nodes.
+        self.generic_visit(node)
+
+    def visit_TestCaseName(self, node):
+        print(f"- {node.name} (on line {node.lineno})")
+
+
+# want test suite builder, I guess, and want to reimplement the runner, pretty much
+class TestGetSuite(unittest.TestCase):
+    def test_get_tests(self):
+        a, b = extract_test_cases_from_ref("/Users/jdanek/IdeaProjects/ods-ci/ods_ci", None)
+        print(a, b)
+
+    def test_ast(self):
+        import robot.api
+        model = robot.api.get_model(
+            "/Users/jdanek/IdeaProjects/ods-ci/ods_ci/tests/Tests/0500__ide/0501__ide_jupyterhub/image-iteration.robot")
+        print(model)
+        printer = TestNamePrinter()
+        printer.visit(model)
+        print(model.sections[0])
+        print(ast.dump(model))
+
+    def test_build_suite(self):
+        REPO_ROOT = pathlib.Path(__file__).parent.parent.parent.parent
+        builder = TestSuiteBuilder()
+        testsuite = builder.build(REPO_ROOT / "ods_ci" / "tests/")
+
+        SuiteRunner(testsuite).run()
+
+    def test_quickstart_example1(self):
+        """https://github.com/robotframework/QuickStartGuide/blob/master/QuickStart.rst#workflow-tests"""
+        sources = """*** Test Cases ***
+User can create an account and log in
+    Create Valid User    fred    P4ssw0rd
+    Attempt to Login with Credentials    fred    P4ssw0rd
+    Status Should Be    Logged In
+
+User cannot log in with bad password
+    Create Valid User    betty    P4ssw0rd
+    Attempt to Login with Credentials    betty    wrong
+    Status Should Be    Access Denied"""
+
+        testsuite = suite = robot.api.TestSuite.from_string(sources)
+        runner = SuiteRunner(testsuite)
+        runner.run()
+
+        assert runner.get_python_code() == """def User_can_create_an_account_and_log_in():
+    Create_Valid_User('fred', 'P4ssw0rd')
+    Attempt_to_Login_with_Credentials('fred', 'P4ssw0rd')
+    Status_Should_Be('Logged In')
+
+
+def User_cannot_log_in_with_bad_password():
+    Create_Valid_User('betty', 'P4ssw0rd')
+    Attempt_to_Login_with_Credentials('betty', 'wrong')
+    Status_Should_Be('Access Denied')
+"""
+
+
+def format_functionname(name: str) -> str:
+    name = name.translate(str.maketrans(' -', '__', '"'))
+    return name
+
+def format_assignment(value: str) -> str:
+    if (m := re.match(r'^[$@&]\{([^{]+)}\s*=?\s*$', value)) is not None:
+        return m.group(1)
+    raise ValueError(value)
+
+def format_argument(value: str) -> str:
+    if not value:
+        return value
+    if re.match(r'^[$@]\{[^{]+}$', value):
+        return value[2:-1]
+    if re.search(r'[$@]\{', value):
+        return "f'" + value.replace("${", "{").replace("@{", "{") + "'"
+    if value[0] in ("'", '"'):
+        return value
+    return f"'{value}'"
+
+
+class CodeWriter():
+    def __init__(self):
+        self.buffer = io.StringIO()
+        self.indent = 0
+
+    def begin(self, line: str):
+        self.buffer.write((" " * 4 * self.indent) + line + "\n")
+        self.indent += 1
+
+    def add(self, line: str):
+        self.buffer.write((" " * 4 * self.indent) + line + "\n")
+
+    def end(self):
+        self.indent -= 1
+
+    def print(self):
+        print(self.buffer.getvalue())
+
+
+class SuiteRunner(SuiteVisitor):
+    def __init__(self, testsuite):
+        self.testsuite = testsuite
+        self.generated_test_methods: list[str] = []
+
+    def get_python_code(self):
+        return '\n\n'.join(self.generated_test_methods)
+
+    def run(self):
+        self.testsuite.visit(self)
+
+    def start_suite(self, suite: robot.running.model.TestSuite):
+        settings = robot.conf.settings.RobotSettings()
+        variables = robot.variables.scopes.VariableScopes(settings)
+        ns = robot.running.namespace.Namespace(variables, suite, suite.resource, languages=None)
+        print(suite.resource.variables)
+        print(f"Starting suite '{suite.name}'")
+        return "a"
+
+    def end_suite(self, suite: robot.running.model.TestSuite):
+        print(f"Ending suite '{suite.name}'")
+        return "b"
+
+    def visit_test(self, test: robot.running.model.TestCase):
+        cw = CodeWriter()
+        cw.begin(f"def {test.name.replace(' ', '_')}():")
+
+        print(f"Visiting test '{test.name}' {type(test.body)}")
+        body: robot.running.model.Body = test.body
+
+        self.translate_body(body, cw)
+        cw.end()
+        self.generated_test_methods.append(cw.buffer.getvalue())
+
+        cw.print()
+
+    def visit_keyword(self, keyword: robot.running.model.Keyword):
+        if not hasattr(keyword, 'body'):
+            return
+        cw = CodeWriter()
+        cw.begin(f"def {keyword.name.replace(' ', '_')}():")
+
+        print(f"Visiting keyword '{keyword.name}' {type(keyword.body)}")
+        body: robot.running.model.Body = keyword.body
+
+        self.translate_body(body, cw)
+        cw.end()
+        self.generated_test_methods.append(cw.buffer.getvalue())
+
+        cw.print()
+
+    def translate_body(self, body: robot.running.model.Body, cw: CodeWriter):
+        for x in body:
+            match x:
+                case robot.running.model.Keyword():
+                    print("keyword", x.body if hasattr(x, 'body') else "")
+                    x: robot.running.model.Keyword
+                    name = format_functionname(x.name)
+                    args = []
+                    kwargs = {}
+                    for arg in x.args:
+                        parts = arg.split('=', maxsplit=1)
+                        if len(parts) == 1:
+                            args.append(format_argument(parts[0]))
+                        else:
+                            kwargs[parts[0]] = format_argument(parts[1])
+                    expression = f"{name}({', '.join(args)}{',' if args and kwargs else ''}{', '.join(k + '=' + v for k, v in kwargs.items())})"
+                    if x.assign:
+                        lhs = ', '.join([format_assignment(arg) for arg in x.assign])
+                        cw.add(f"{lhs} = {expression}")
+                    else:
+                        cw.add(expression)
+                case robot.running.model.If():
+                    print("if")
+                    for y in x.body:
+                        match y:
+                            case robot.running.model.IfBranch():
+                                print("ifbranch", y.type, y.condition, y.body)
+                                if y.type == "IF":
+                                    cw.begin(f"if {y.condition}:")
+                                elif y.type == "ELSE IF":
+                                    cw.begin(f"elif {y.condition}:")
+                                elif y.type == "ELSE":
+                                    cw.begin(f"else:")
+                                else:
+                                    raise Exception(f"Unexpected type '{y.type}'")
+
+                                self.translate_body(y.body, cw)
+
+                                cw.end()
+
+                case robot.running.model.For():
+                    print("for")
+                case default:
+                    raise Exception(f"default {type(x)}")
+
+
+class Context:
+    def get_runner(self, _):
+        return BodyRunner(self)
+
+    @property
+    def dry_run(self):
+        return False
+
+
+class BodyRunner:
+
+    def __init__(self, context, run=True, templated=False):
+        self._context = context
+        self._run = run
+        self._templated = templated
+
+    def run(self, body, *args, **kwargs):
+        print("body", body)
+        errors = []
+        passed = None
+        for step in iter(body):
+            # try:
+            step.run(self._context, self._run, self._templated)
+        # except ExecutionPassed as exception:
+        #     exception.set_earlier_failures(errors)
+        #     passed = exception
+        #     self._run = False
+        # except ExecutionFailed as exception:
+        #     errors.extend(exception.get_errors())
+        #     self._run = exception.can_continue(self._context, self._templated)
+        if passed:
+            raise passed
+        # if errors:
+        #     raise ExecutionFailures(errors)
